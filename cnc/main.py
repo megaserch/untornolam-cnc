31164ecaa0e2079076c7bc1842d9aec4351b9ba7
guidovigiani <guidovigiani@gmail.com>
2020-12-07 18:06:57 -0300

    debugg 4

--- cnc/main.py
+++ cnc/main.py
@@ -82,9 +82,11 @@ def connect():
     soquete.emit('control message', 'Enc_SinCal')
     time.sleep(1)
     soquete.emit('control message', 'Calibrando')
+    time.sleep(2)
     do_line("G28")
-    time.sleep(5)
+    time.sleep(2)
     soquete.emit('control message', 'Enc_Calibr')
+    time.sleep(5)
 
 @soquete.event
 def connect_error():
