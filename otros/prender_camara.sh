c87c365469b7e0a1dee0c1db7d9f657a5cee141b
megaserch <megaserch@gmail.com>
2020-12-12 09:30:42 -0300

    camara

--- otros/prender_camara.sh
+++ otros/prender_camara.sh
@@ -1,1 +1,2 @@
-screen -dmS camara-torno /home/pi/mjpg-streamer/mjpg-streamer-experimental/mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -fps 30"
+cd /home/pi/mjpg-streamer/mjpg-streamer-experimental
+screen -dmS camara-torno ./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x 320 -y 240 -fps 30"
